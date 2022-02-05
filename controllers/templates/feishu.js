8ad45280d516cf92d4e047da5a59b8ae3cf37bdb
Yuancheng Zhang <old.far.hot@gmail.com>
2022-02-05 13:47:08 +0800

    去掉输出

--- controllers/templates/feishu.js
+++ controllers/templates/feishu.js
@@ -65,10 +65,10 @@ const options = {
 const sendRequest = (bot, msg) => {
   options.path = path + bot;
   const req = https.request(options, (res) => {
-    console.log("statusCode:", res.statusCode);
+    // console.log("statusCode:", res.statusCode);
     // console.log("headers:", res.headers);
     res.on("data", (d) => {
-      process.stdout.write(d);
+      //   process.stdout.write(d);
     });
   });
 
